669b2f3905a7f9bdaf4797e701c7ab12b75800fd
juanframart2011 <juanframart2011@gmail.com>
2025-02-26 19:10:31 -0600

    ajuste de cambios de env

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # Configuración de correo para recuperación de contraseña
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
